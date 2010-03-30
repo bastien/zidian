5d31bdaee9670ac219483db10fd4601df67699e7
Bastien Vaucher <bastien.vaucher@gmail.com>
2010-03-31 00:03:21 +0200

    initial commit

---
+++ lib/zidian.rb
@@ -0,0 +1,33 @@
+module Zidian
+  
+  def self.find(expression)
+    $KCODE = 'UTF8'
+    case expression.class.name
+    when "Integer", "Fixnum" then
+      line = `sed -n '#{expression}p' lib/cedict_ts.u8`
+      extract(line)
+    when "String" then
+      lines = `less lib/cedict_ts.u8 | grep -n #{expression}`
+      lines.lines.to_a.collect{|line| extract(line) }
+    else
+      raise "Invalid find parameter(#{expression.class}). Only integers, strings accepted"
+    end
+  end
+  
+  protected
+  
+  def self.extract(line, line_number = nil)
+    line.strip!
+    if line =~ /^[0-9]*:/
+      line_number = line.gsub!(/^[0-9]*:/).to_a.first.gsub(':','').to_i
+    end
+    {
+      :id => line_number,
+      :traditional => line.match(/^[^\s]+/)[0],
+      :simplified => line.match(/\s[^\s]+/)[0].strip,
+      :pinyin  => line.match(/\[.+?\]/)[0].gsub(/[\[\]]/,''),
+      :english => line.scan(/\/[^\/]+/).collect{|e| e.gsub(/[\/]/,'')}
+    }
+  end
+  
+end
